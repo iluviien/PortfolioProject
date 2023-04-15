abdf5ec13adb51a015a22e6014ae7d9ec2f707c9
iluviien <stefanica695@gmail.com>
2023-04-15 12:54:42 +0000

    add

--- my-portfolio/src/strapi/MessageBoard.js
+++ my-portfolio/src/strapi/MessageBoard.js
@@ -1,39 +1,83 @@
+import React, { useState } from "react";
 import "./MessageBoard.css";
 import {
-  Box,
+  Card,
+  CardBody,
+  //Box,
   VStack,
   Input,
   Textarea,
-  HStack,
+  // HStack,
   Button,
-  Text
+  Text,
+  //Stack,
+  Avatar,
+  //Divider,
+  Flex
+  //Spacer,
+  //IconButton,
+  //Tooltip,
+  //useToast
 } from "@chakra-ui/react";
 
 function MessageBoard(props) {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   return (
-    <div className="containerDaddy">
-      <Box
-        justifyContent="center"
-        m="auto"
-        borderRadius="md"
-        my={6}
-        w="1000"
-        h="300px"
-      >
-        <HStack>
-          <VStack>
-            <Input placeholder="Name" type="text" />
-            <Textarea placeholder="Type message here"></Textarea>
-          </VStack>
-          <Button bg="blue" fontSize={"10px"}>
-            Cick me biatch
+    <Card w="1000px">
+      <CardBody>
+        <VStack justifyContent="left">
+          <Input
+            w="150px"
+            placeholder="Name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            mb={3}
+          />
+
+          <Textarea
+            w="400px"
+            placeholder="Message"
+            value={message}
+            onChange={e => setMessage(e.target.value)}
+            mb={3}
+          />
+          <Button
+            colorScheme="purple"
+            onClick={() => {
+              const newMessage = { name: name, message: message };
+              setMessages([...messages, newMessage]);
+              setName("");
+              setMessage("");
+            }}
+          >
+            Submit
           </Button>
-          <Box p={3} borderRadius="md" w="650px" h="300px" bg="blue">
-            <Text>CHARLEYYYYY</Text>
-          </Box>
-        </HStack>
-      </Box>
-    </div>
+        </VStack>
+      </CardBody>
+
+      <VStack spacing={4}>
+        {messages.map((message, index) => (
+          <CardBody key={index} bg="white" boxShadow="md" p={4} rounded="md">
+            <Flex>
+              <Avatar
+                size="sm"
+                name={message.name}
+                src="images/catAvatar.jpg"
+                mr={2}
+              />
+              <Text fontWeight="bold">{message.name}</Text>
+            </Flex>
+            <Text p={2} mt="5px">
+              {message.message}
+            </Text>
+          </CardBody>
+        ))}
+      </VStack>
+    </Card>
   );
 }
+
 export default MessageBoard;
